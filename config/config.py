d38c143c5932f84470b17cd93dfe2953dcb6196d
YuWang(Steven) <daaiwusheng003@gmail.com>
2021-11-15 23:36:01 +0000

    mac上切片无法show,只在show函数里,不影响主体功能

--- config/config.py
+++ config/config.py
@@ -19,6 +19,7 @@ RESULT_DIR = "F:/LeedsDocs/Kaggle/kaggle_result/"  # On Tianyou's pc
 
 # MASK_DIR = "/Users/wangyu/Desktop/利兹上课资料/Kaggle比赛/data/train_mask.csv" # Steven Mac
 # TRAIN_CSV = "/Users/wangyu/Desktop/利兹上课资料/Kaggle比赛/data/train.csv" # Steven Mac
+# TRAIN_PATH = "/Users/wangyu/Desktop/利兹上课资料/Kaggle比赛/data/train/" # Steven Mac
 
 # TRAIN_CSV = "D:/Seg-cell/train.csv" # guanhui
 # TRAIN_PATH = "D:/Seg-cell/train" # guanhui
--- testcode.py
+++ testcode.py
@@ -2,9 +2,9 @@ from dataprocess.train_labels_process import *
 from dataprocess.train_image_process import *
 from dataprocess.kaggle_data_provider import *
 
-# KaggleDataProvider()
+KaggleDataProvider()
 
-train_processor = TrainLabelsProcessor()
+# train_processor = TrainLabelsProcessor()
 # print(len(train_processor))
 
 # train_img_processor = TrainImageHandler()
