7ab12cf6ee5c6467fe2e8511424a8fdefca63f65
Tianyou Wei <35444042+moshen2888@users.noreply.github.com>
2021-11-15 23:26:53 +0000

    注释

--- config/config.py
+++ config/config.py
@@ -6,16 +6,16 @@
 # RESULT_DIR = "F:/LeedsDocs/Kaggle/kaggle_result/"  # On Tianyou's pc
 
 
-# TRAIN_CSV = "F:/LeedsDocs/Kaggle/sartorius-cell-instance-segmentation/train.csv"
-# TRAIN_PATH = "F:/LeedsDocs/Kaggle/sartorius-cell-instance-segmentation/train/"
-# MASK_DIR = "F:/LeedsDocs/Kaggle/sartorius-cell-instance-segmentation/train_mask.csv"
-# RESULT_DIR = "F:/LeedsDocs/Kaggle/kaggle_result/"  # On Tianyou's pc
+TRAIN_CSV = "F:/LeedsDocs/Kaggle/sartorius-cell-instance-segmentation/train.csv"
+TRAIN_PATH = "F:/LeedsDocs/Kaggle/sartorius-cell-instance-segmentation/train/"
+MASK_DIR = "F:/LeedsDocs/Kaggle/sartorius-cell-instance-segmentation/train_mask.csv"
+RESULT_DIR = "F:/LeedsDocs/Kaggle/kaggle_result/"  # On Tianyou's pc
 
 
-TRAIN_CSV = "/content/drive/MyDrive/Project/sartorius-cell-instance-segmentation/train.csv"
-TRAIN_PATH = "/content/drive/MyDrive/Project/sartorius-cell-instance-segmentation/train/"
-MASK_DIR = "/content/drive/MyDrive/Project/sartorius-cell-instance-segmentation/train_mask.csv"
-RESULT_DIR = "/content/drive/MyDrive/Project/kaggle_result/"  # On Colab
+# TRAIN_CSV = "/content/drive/MyDrive/Project/sartorius-cell-instance-segmentation/train.csv"
+# TRAIN_PATH = "/content/drive/MyDrive/Project/sartorius-cell-instance-segmentation/train/"
+# MASK_DIR = "/content/drive/MyDrive/Project/sartorius-cell-instance-segmentation/train_mask.csv"
+# RESULT_DIR = "/content/drive/MyDrive/Project/kaggle_result/"  # On Colab
 
 # TRAIN_CSV = "D:/Seg-cell/train.csv" # guanhui
 # TRAIN_PATH = "D:/Seg-cell/train" # guanhui
